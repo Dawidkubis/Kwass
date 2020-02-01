c09cc37532963938f21a32c1b306983e91dae031
Dawid J. Kubis <dawid.kubis3@gmail.com>
2020-02-01 23:58:26 +0100

    this is harder that it seemed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+mod tokens;
+
+use tokens::Interpreter;
+
+use structopt::StructOpt;
+use anyhow::Result;
+
+use std::fs::read_to_string;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+	/// file to process
+	file: String,
+}
+
+fn main() -> Result<()>{
+	let opt = Opt::from_args();
+
+	let input: Vec<String> = read_to_string(opt.file)?
+		.lines()
+		.map(String::from)
+		.collect();
+
+	for i in input {
+		if let Some(s) = Interpreter::interpret(i) {
+			print!("{}", s);
+		}
+	}
+
+	Ok(())
 }
---
+++ src/tokens/mod.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+
+pub struct Interpreter {
+	scope: Scope,
+}
+
+impl Interpreter {
+	pub fn interpret(&mut self, line: String) -> Option<String> {
+		//TODO
+	}
+}
+
